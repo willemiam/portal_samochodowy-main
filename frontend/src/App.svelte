ad77dba0fd21c35efe2c0f7bb55221f6019a8fdf
willem.b <56897328+willemiam@users.noreply.github.com>
2025-05-03 13:19:54 +0200

    Revert "feature-login implementation"

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,18 +1,11 @@
 <script>
   import { Router, Route } from "svelte-routing";
-  import { onMount } from "svelte";
-  import auth from "./authService";
-  import { isAuthenticated, user } from "./stores/store";
   import Home from "./routes/Home.svelte";
   import Items from "./routes/items.svelte";
   import Filter from "./routes/filter.svelte";
   // import Kontakt from "./routes/Kontakt.svelte";
   import Navbar from "./components/Navbar01.svelte";
   import Account from "./routes/account.svelte"
-
-  onMount(async () => {
-    await auth.checkAuth();
-  })
 </script>
 
 <Router>
--- frontend/src/auth_config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-    domain:  import.meta.env.VITE_AUTH0_DOMAIN,
-    clientId: import.meta.env.VITE_AUTH0_CLIENT_ID
-}
-
-console.log('Auth Config Loaded:', config);
-
-export default config;
